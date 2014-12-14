59b6d5b7e4f337320ea12d381e9cad0aa9c9fa75
Malcolm Inglis <me@minglis.id.au>
2014-12-14 13:01:59 +1000

    Fix 'unused variable' warning on fast build

--- tests/slice.c
+++ tests/slice.c
@@ -17,6 +17,7 @@ int main( void )
             ws[ 1 ] == xs[ 4 ],
             ws[ 2 ] == xs[ 5 ],
             ws[ 3 ] == xs[ 6 ] );
+    ( void ) ws;
 
     printf( "Testing total slice...\n" );
     int const ys[] = { SLICE( xs, 0, 6 ) };
@@ -27,10 +28,12 @@ int main( void )
             ys[ 3 ] == xs[ 3 ],
             ys[ 4 ] == xs[ 4 ],
             ys[ 5 ] == xs[ 5 ] );
+    ( void ) ys;
 
     printf( "Testing empty slice...\n" );
     int const zs[] = { 0, SLICE( xs, 2, 0 ) };
     ASSERT( NELEM( zs ) == 1 );
+    ( void ) zs;
 
     printf( "SLICE() tests passed.\n" );
 }
